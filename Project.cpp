226ed78e9b2f696c69ceea5f95c85c2514b429bd
Ruzzell <Ruzzell-O@proton.me>
2022-10-21 08:42:27 +0800

    0.1

---
+++ Project.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Thing needed to do
+    /*
+    Conditional Statements
+    Multidimentional Arrays
+    Mathematical Operations
+    Classes and Objects
+    */
+
+// Program Flow
+    /*
+    1. The program will ask how much is in thier balance
+    2. Then will present the products that are available
+    3. The user will be prompted to select the products that are available
+    4. Then will the program will calculate remaining balance
+    */
+
+// Products
+    /*
+        Open Word
+            Action
+                Elden Ring
+                God of War
+                New World
+            RPG
+                Cyberpunk
+                Red Dead Redemption 2
+                GTA V
+            Survival
+                The Forest
+                Dying Light 2 Stay Human
+                Terraria
+
+        Horror
+            Co-op
+                Dead by Daylight
+                Phasmophobia
+                Grounded
+            Single Player
+                Alien: Isolation
+                Little Nightmares 2
+                Outlast 2
+            Pychological Horror
+                Omori
+                Doki Doki Literature Club
+                Fran Bow
+
+        Simulation
+            Relaxing
+                Slime Rancher 2
+                The Sims 4
+                Disney Dreamlight Valley
+            Management
+                Potionomics
+                Civilization VI
+                Planet Zoo
+            Sports
+                FIFA23
+                NBA 2K23
+                PGA TOUR 2K23
+    */
+
+int main(){
+
+// Not much proggress as this is moment due to I have scratch my privious working
+
+}
